8eca23f172979b2b77bcd7ec3b6c64f4da7e0fa5
fakhrad <fakhrad@openratio.com>
2019-04-09 19:12:02 +0430

    controller updated for new routing methods

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -82,3 +82,133 @@ exports.register = [
         };
     }
 ];
+
+
+exports.changeavatar = [
+    (req, res, next) =>{
+        var errors = validationResult(req);
+        if (!errors.isEmpty())
+        {  
+            //There are errors. send error result
+            res.status(400).json({"success" : false, "error" : errors});
+            return;
+        }
+        else
+        {
+            console.log('add system user started.')
+            broker.sendRPCMessage(req.body, 'adminchangeavatar').then((result)=>{
+                var obj = JSON.parse(result.toString('utf8'));
+                if (!obj.success)
+                {
+                    if (obj.error)
+                    {
+                        return res.status(500).json(obj);
+                    }
+                }
+                else
+                {
+                    res.status(201).json(wrapUser(obj.data));
+                }
+            });
+        };
+    }
+];
+
+
+exports.updateprofile = [
+    (req, res, next) =>{
+        var errors = validationResult(req);
+        if (!errors.isEmpty())
+        {  
+            //There are errors. send error result
+            res.status(400).json({"success" : false, "error" : errors});
+            return;
+        }
+        else
+        {
+            console.log('add system user started.')
+            broker.sendRPCMessage(req.body, 'adminupdateprofile').then((result)=>{
+                var obj = JSON.parse(result.toString('utf8'));
+                if (!obj.success)
+                {
+                    if (obj.error)
+                    {
+                        return res.status(500).json(obj);
+                    }
+                }
+                else
+                {
+                    res.status(201).json(wrapUser(obj.data));
+                }
+            });
+        };
+    }
+];
+
+
+exports.deleteAccount = [
+    (req, res, next) =>{
+        var errors = validationResult(req);
+        if (!errors.isEmpty())
+        {  
+            //There are errors. send error result
+            res.status(400).json({"success" : false, "error" : errors});
+            return;
+        }
+        else
+        {
+            console.log('add system user started.')
+            broker.sendRPCMessage(req.body, 'admindeleteaccount').then((result)=>{
+                var obj = JSON.parse(result.toString('utf8'));
+                if (!obj.success)
+                {
+                    if (obj.error)
+                    {
+                        return res.status(500).json(obj);
+                    }
+                }
+                else
+                {
+                    res.status(201).json(wrapUser(obj.data));
+                }
+            });
+        };
+    }
+];
+
+
+exports.findbyemail = [
+    // body('username', "Username must not be empty").isEmail().withMessage('Invalid email address.'),
+    // body('password', "Password must not be empty").isLength({min : 8}).withMessage('Password length must be at least 8 charachters').isAlphanumeric().withMessage('Password must contain charachters and numbers'),
+    // //Sanitize fields
+    // sanitizeBody('email').trim().escape(),
+    // sanitizeBody('password').trim().escape(),
+    (req, res, next) =>{
+        var errors = validationResult(req);
+        if (!errors.isEmpty())
+        {  
+            //There are errors. send error result
+            res.status(400).json({"success" : false, "error" : errors});
+            return;
+        }
+        else
+        {
+            console.log('add system user started.')
+            broker.sendRPCMessage(req.body, 'adminfindbyemail').then((result)=>{
+                var obj = JSON.parse(result.toString('utf8'));
+                if (!obj.success)
+                {
+                    if (obj.error)
+                    {
+                        return res.status(500).json(obj);
+                    }
+                }
+                else
+                {
+                    res.status(201).json(wrapUser(obj.data));
+                }
+            });
+        };
+    }
+];
+
