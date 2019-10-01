bca4928dcaf5f9de050014e22b6191143c0b724c
fakhrad <fakhrad@openratio.com>
2019-10-01 10:59:27 +0330

    get by link added

--- src/controllers/contentController.js
+++ src/controllers/contentController.js
@@ -42,6 +42,28 @@ exports.getById = function(req, res, next) {
     });
 };
 
+exports.getByLink = function(req, res, next) {
+  broker
+    .sendRPCMessage(
+      {
+        spaceId: req.spaceid,
+        userId: req.userId,
+        body: { link: req.query.link }
+      },
+      "getcontentbylink"
+    )
+    .then(result => {
+      var obj = JSON.parse(result.toString("utf8"));
+      if (!obj.success) {
+        if (obj.error) return res.status(500).json(obj);
+        else {
+          res.status(404).json(obj);
+        }
+      } else {
+        res.status(200).json(obj.data);
+      }
+    });
+};
 exports.add = function(req, res, next) {
   broker
     .sendRPCMessage(
