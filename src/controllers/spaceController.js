68141328cf9bf65fad2a3a7b5dd34e70b076a552
fakhrad <fakhrad@openratio.com>
2019-10-06 14:13:31 +0330

    set roles added to space routing

--- src/controllers/spaceController.js
+++ src/controllers/spaceController.js
@@ -1,69 +1,72 @@
-var jwt = require('jsonwebtoken');
-const config = require('../config');
-const { body, validationResult } = require('express-validator/check');
-const { sanitizeBody } = require('express-validator/filter');
-const broker = require('./serviceBroker');
-exports.stats = function(req, res, next)
-{
+var jwt = require("jsonwebtoken");
+const config = require("../config");
+const { body, validationResult } = require("express-validator/check");
+const { sanitizeBody } = require("express-validator/filter");
+const broker = require("./serviceBroker");
+exports.stats = function(req, res, next) {};
 
-}
-
-exports.setlocales = function(req, res, next)
-{
-    broker.sendRPCMessage({userId : req.userId, body : req.body}, 'setspacelocales').then((result)=>{
-        var obj = JSON.parse(result.toString('utf8'));
-        if (!obj.success)
-        {
-            if (obj.error)
-                return res.status(500).json(obj);
-            else
-            {
-                res.status(404).json(obj);
-            }
+exports.setlocales = function(req, res, next) {
+  broker
+    .sendRPCMessage({ userId: req.userId, body: req.body }, "setspacelocales")
+    .then(result => {
+      var obj = JSON.parse(result.toString("utf8"));
+      if (!obj.success) {
+        if (obj.error) return res.status(500).json(obj);
+        else {
+          res.status(404).json(obj);
         }
-        else
-        {
-            res.status(200).json(obj.data);
-        }
+      } else {
+        res.status(200).json(obj.data);
+      }
     });
-}
+};
 
-exports.setwebhooks = function(req, res, next)
-{
-    broker.sendRPCMessage({userId : req.userId, body : req.body}, 'setspacewebhooks').then((result)=>{
-        var obj = JSON.parse(result.toString('utf8'));
-        if (!obj.success)
-        {
-            if (obj.error)
-                return res.status(500).json(obj);
-            else
-            {
-                res.status(404).json(obj);
-            }
+exports.setroles = function(req, res, next) {
+  broker
+    .sendRPCMessage({ userId: req.userId, body: req.body }, "setspaceroles")
+    .then(result => {
+      var obj = JSON.parse(result.toString("utf8"));
+      if (!obj.success) {
+        if (obj.error) return res.status(500).json(obj);
+        else {
+          res.status(404).json(obj);
         }
-        else
-        {
-            res.status(200).json(obj.data);
-        }
+      } else {
+        res.status(200).json(obj.data);
+      }
     });
-}
+};
+exports.setwebhooks = function(req, res, next) {
+  broker
+    .sendRPCMessage({ userId: req.userId, body: req.body }, "setspacewebhooks")
+    .then(result => {
+      var obj = JSON.parse(result.toString("utf8"));
+      if (!obj.success) {
+        if (obj.error) return res.status(500).json(obj);
+        else {
+          res.status(404).json(obj);
+        }
+      } else {
+        res.status(200).json(obj.data);
+      }
+    });
+};
 
-exports.getwebhooks = function(req, res, next)
-{
-    broker.sendRPCMessage({userId : req.userId, body : {id : req.query.id}}, 'getspacewebhooks').then((result)=>{
-        var obj = JSON.parse(result.toString('utf8'));
-        if (!obj.success)
-        {
-            if (obj.error)
-                return res.status(500).json(obj);
-            else
-            {
-                res.status(404).json(obj);
-            }
+exports.getwebhooks = function(req, res, next) {
+  broker
+    .sendRPCMessage(
+      { userId: req.userId, body: { id: req.query.id } },
+      "getspacewebhooks"
+    )
+    .then(result => {
+      var obj = JSON.parse(result.toString("utf8"));
+      if (!obj.success) {
+        if (obj.error) return res.status(500).json(obj);
+        else {
+          res.status(404).json(obj);
         }
-        else
-        {
-            res.status(200).json(obj.data);
-        }
+      } else {
+        res.status(200).json(obj.data);
+      }
     });
-}
+};
--- src/routes/spaces.js
+++ src/routes/spaces.js
@@ -1,11 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var auth = require('../controllers/auth');
-var controller = require('../controllers/spaceController');
+var auth = require("../controllers/auth");
+var controller = require("../controllers/spaceController");
 
 router.get("/stats", auth.verifyToken, controller.stats);
 
 router.put("/setlocales", auth.verifyToken, controller.setlocales);
+router.put("/setroles", auth.verifyToken, controller.setroles);
 router.put("/setwebhooks", auth.verifyToken, controller.setwebhooks);
 router.get("/getwebhooks", auth.verifyToken, controller.getwebhooks);
 
